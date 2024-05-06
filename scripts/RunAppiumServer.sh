162f1d59411a6f2994b8cc5b5e14c0a659ef5084
khaledBEQA <bedouikhaled090@gmail.com>
2024-05-06 10:40:00 +0100

    First commit

---
+++ scripts/RunAppiumServer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# Installation du SDK Android (si ce n'est pas déjà fait)
+# Assurez-vous que les variables d'environnement ANDROID_HOME et PATH sont correctement configurées
+# En supposant que le SDK Android est déjà installé et configuré sur votre système
+
+# Installer Appium et les pilotes Android
+npm install -g appium@next
+appium driver install uiautomator2
+appium driver install espresso
+appium driver install flutter
+
+# Vérifier la version d'Appium installée
+appium -v
+
+# Démarrer le serveur Appium en arrière-plan et rediriger les logs vers un fichier
+appium --log appium.log &>/dev/null &
