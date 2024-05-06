7cbd0251207046482946b69aae36fe3546936571
khaledBEQA <bedouikhaled090@gmail.com>
2024-05-06 15:58:30 +0100

    First commit

--- src/main/java/com/Appium2_0_Project/Base.java
+++ src/main/java/com/Appium2_0_Project/Base.java
@@ -42,7 +42,7 @@ public class Base {
         DesiredCapabilities des =new DesiredCapabilities();
         des.setCapability(MobileCapabilityType.PLATFORM_NAME,platformName);
         des.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,300);
-        URL url = new URL("http://192.168.2.23:4723/wd/hub");
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
 
         switch (platformName) {
@@ -56,6 +56,7 @@ public class Base {
                 des.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_5");
                 des.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
                 des.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+                des.setCapability("isHeadless",true);
                 des.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "io.appium.android.apis");
                 des.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "io.appium.android.apis.ApiDemos");
                 des.setCapability(AndroidMobileCapabilityType.AVD, "Pixel_5");
